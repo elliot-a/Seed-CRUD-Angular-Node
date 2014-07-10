466e69dcd9982e4526b5a1240ffacf7c5fa84efd
Elliot A <elliot@flashtalking.com>
2014-07-10 17:11:26 +0100

    fixing bug

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp        = require('gulp');
 var gutil       = require('gulp-util');
 var jshint      = require('gulp-jshint');
 var browserify  = require('gulp-browserify');
-var sourcemaps  = require('gulp-sourcemaps');
 var sass        = require('gulp-sass');
 var rimraf      = require('rimraf');
 
